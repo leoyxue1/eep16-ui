01aa4f4ab517287a428a7b025f5489c7e7bf017e
timtlchen <cjh0313@163.com>
2018-11-23 16:19:51 +0800

    fix the deploy command

--- ssh_deploy.sh
+++ ssh_deploy.sh
@@ -9,15 +9,9 @@ export http_proxy=http://127.0.0.1:1080
 export https_proxy=http://127.0.0.1:1080
 export no_proxy="127.0.0.1, localhost, 192.168.*.*"
 
-app_name="eep16-ui"
-#echo "Creating temporary tar file for deploymment ..."
-#temp_file="/c/temp/${app_name}.tar"
-#rm -f "${temp_file}"
-#tar --exclude='./venv' --exclude='./.idea' --exclude='./__pycache__' -cf "${temp_file}" .
-
 target_ip=$(gcloud compute instances list --filter="NAME:eep16-ui" --format='[box]' | grep RUNNING | awk -F\| '{print $7}')
 
-git_cmd="sudo mkdir -pv /eep16_apps/; sudo chown -R appadm:appadm /eep16_apps/; cd /eep16_apps/; rm -rf /eep16_apps/${app_name}; git clone https://github.com/timtlchen/eep16-ui.git; cd /eep16_apps/${app_name}; sh conda_start.sh 2>&1 ; exit"
+git_cmd="sudo mkdir -pv /eep16_apps/; sudo chown -R appadm:appadm /eep16_apps/; cd /eep16_apps/; rm -rf /eep16_apps/eep16-ui; git clone https://github.com/timtlchen/eep16-ui.git; cd /eep16_apps/eep16-ui; sh conda_start.sh 2>&1 ; exit"
 for ip in ${target_ip}
 do
     echo "Deploying to target: ${ip}"
